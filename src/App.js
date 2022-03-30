6249f8eb73815d002b6f051230b6cd73721e4d6e
furkandindar <fdindar1@binghamton.edu>
2022-03-30 11:38:52 +0300

    implement scrollintoview

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Navbar from './Navbar';
 import { useTheme } from '@mui/material/styles';
@@ -11,21 +12,194 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import { useRef } from 'react';
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import MenuItem from '@mui/material/MenuItem';
+
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+const pages = ['Home', 'About', 'Roadmap', 'Team', 'Whitepaper'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function App() {
+  const [value, setValue] = React.useState('female');
 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const aboutRef = useRef(null);
+  const homeRef = useRef(null);
+  const roadmapRef = useRef(null);
+  const teamRef = useRef(null);
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('md'));
+  const matches = useMediaQuery(theme.breakpoints.up('lg'));
   return (
     <Grid container height={"100vh"} sx={{position:"relative"}}>
-      <Grid item sx={{zIndex:10}} xs={12}>
-        <Navbar/>
+      <Grid ref={homeRef} item sx={{zIndex:10}} xs={12}>
+      <AppBar position="static"  sx={{background:"transparent", boxShadow:"none"}}>
+      <Container maxWidth="xl" >
+        <Toolbar disableGutters >
+          <Grid container>
+              <Grid item container alignItems={"center"} xs={12}>
+                <Box
+                    sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                >
+                    <img src="logo-md.png" width={"131px"}/>
+                </Box>
+
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                    >
+                    <MenuIcon />
+                    </IconButton>
+                    <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                        display: { xs: 'block', md: 'none' },
+                    }}
+                    >
+                    {pages.map((page) => (
+                        <MenuItem key={page} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">{page}</Typography>
+                        </MenuItem>
+                    ))}
+                    </Menu>
+                </Box>
+                {/* <Box
+                    sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                >
+                    <img src="logo-md.png" width={"100px"}/>
+                </Box> */}
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent:"center" }}>
+                    {/* {pages.map((page) => (
+                    <Box
+                        key={page}
+                        onClick={()=> {aboutRef.current.scrollIntoView({behavior:"smooth"})}}
+                        sx={{ my: 2, mx:3, color: 'white', display: 'block' }}
+                    >
+                        {page}
+                    </Box>
+                    ))} */}
+                    <Box
+                        onClick={()=> {homeRef.current.scrollIntoView({behavior:"smooth"})}}
+                        sx={{ my: 2, mx:3, color: 'white', display: 'block', cursor:"pointer" }}
+                    >
+                        Home
+                    </Box>
+                    <Box
+                        onClick={()=> {aboutRef.current.scrollIntoView({behavior:"smooth"})}}
+                        sx={{ my: 2, mx:3, color: 'white', display: 'block', cursor:"pointer" }}
+                    >
+                        About
+                    </Box>
+                    <Box
+                        onClick={()=> {roadmapRef.current.scrollIntoView({behavior:"smooth"})}}
+                        sx={{ my: 2, mx:3, color: 'white', display: 'block', cursor:"pointer" }}
+                    >
+                        Roadmap
+                    </Box>
+                    <Box
+                        onClick={()=> {teamRef.current.scrollIntoView({behavior:"smooth"})}}
+                        sx={{ my: 2, mx:3, color: 'white', display: 'block', cursor:"pointer" }}
+                    >
+                        Team
+                    </Box>
+                    <a href="https://docs.colossus.studio/" target={"_blank"} style={{textDecoration:"none"}}>
+                    <Box
+                        sx={{ my: 2, mx:3, color: 'white', display: 'block' }}
+                    >
+                        Whitepaper
+                    </Box>
+                    </a>
+                </Box>
+
+                <Box sx={{ flexGrow: 0 }}>
+                    <Stack direction={"row"} spacing={1} height={25}>
+                        <a href='https://colossusstudio.medium.com'>
+                            <img width={25} src='soc-1.webp' alt='medium'/>
+                        </a>
+                        <a href='https://twitter.com/colossusAR'>
+                            <img width={25} src='soc-2.webp' alt='twitter'/>
+                        </a>
+                        <a href='https://discord.com/invite/rf686eGR'>
+                            <img width={25} src='soc-4.webp' alt='discord'/>
+                        </a>
+                        <a href='https://t.me/colossusAR'>
+                            <img width={25} src='soc-3.webp' alt='telegram'/>
+                        </a>
+                    </Stack>
+                </Box>
+              </Grid>
+              <Grid item container xs={12}>
+                  <Box
+                    sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent:"center" }}
+                >
+                    <img src="logo-md.png" width={"100px"}/>
+                </Box>
+              </Grid>
+          </Grid>
+        </Toolbar>
+      </Container>
+    </AppBar>
       </Grid>
+      {/* <Grid item xs={12} position={"fixed"} sx={{background:"red", zIndex:20, top:"50%", transform:"translateY(-50%)", right:"4rem"}} >
+        <FormControl>
+        <FormLabel id="demo-controlled-radio-buttons-group">Gender</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-controlled-radio-buttons-group"
+          name="controlled-radio-buttons-group"
+          value={value}
+          onChange={handleChange}
+        >
+          <FormControlLabel value="female" control={<Radio />} label="Female" />
+          <FormControlLabel value="male" control={<Radio />} label="Male" />
+        </RadioGroup>
+      </FormControl>
+      </Grid> */}
       <Grid item width={matches ? "100vw" : "auto"} height={matches ? "auto" : "100vh"} sx={{position:"fixed", top:0, left:"50%", transform:"translateX(-50%)", zIndex:1}} >
         <video width={matches ? "100%" : "auto"} height={"100%"} style={{verticalAlign:"baseline", objectFit:"contain", transform:"none", transition:"all 0.3s ease", margin:0,padding:0,border:0,outline:0,background:"transparent",}} src='Colossus-Lab-V2-1.mp4' playsInline autoPlay preload='' muted loop></video>
       </Grid>
       <Grid item height={"100vh"} xs={12}></Grid>
-      <Grid item container justifyContent={"center"} xs={12} sx={{zIndex:10, backgroundColor:"black",position:"relative"}} pt={3}>
+      <Grid ref={aboutRef} item container justifyContent={"center"} xs={12} sx={{zIndex:10, backgroundColor:"black",position:"relative"}} pt={3}>
         <Grid item container justifyContent={"center"} xs={12} md={5}>
           <Fade bottom appear>
             <img style={{width:"100%", height:"auto"}} src="logo-md.png"/>
@@ -86,7 +260,7 @@ function App() {
             </Stack>
           </Grid>
         </Grid>
-        <Grid item container xs={12} justifyContent={"center"} pb={15}>
+        <Grid ref={roadmapRef} item container xs={12} justifyContent={"center"} pb={15}>
           <Timeline sx={{color:"white"}} position="alternate">
             <TimelineItem>
               <TimelineSeparator>
@@ -168,7 +342,7 @@ function App() {
             </TimelineItem>
           </Timeline>
         </Grid>
-        <Grid item container xs={12}>
+        <Grid ref={teamRef} item container xs={12}>
           <Grid item container xs={6} md={3} position="relative">
             <img src='author-1-md.png' width={"100%"}/>
             <Box p={2} width={"60%"} maxWidth={"24.5rem"} position="absolute" bottom={"-2rem"} backgroundColor="#282626" zIndex={2}>
